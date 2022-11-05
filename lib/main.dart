6b84b55dd0da9cf573e360e08ca606bbf3b0cc0e
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-11-05 17:44:36 +0200

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_profile/screens/home_screen/home_screen.dart';
-import 'package:flutter_profile/screens/main_screen/main_screen.dart';
- import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'constants.dart';
 
 void main() {
@@ -9,7 +9,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
